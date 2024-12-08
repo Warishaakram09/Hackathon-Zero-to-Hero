d97042b1a55297ccc859f2000580572fd70f7ffc
Warishaakram09 <warishaakram2005@gmail.com>
2024-12-08 12:03:27 -0800

    First commit

---
+++ hackathon-0-warisha/src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from './components/Hero'
+import NewArrival from './components/NewArrival'
+
+function Home() {
+  return (
+    <div className='w-full-2xl mx-auto'>
+
+      <Hero/>
+
+    </div>
+  )
+}
+
+export default Home
